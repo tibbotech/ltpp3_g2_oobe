58a1b115bd7d2fdaf6d269f2e8d083c297701434
Luis Ramirez <luis@tibbo.com>
2021-03-31 10:58:32 +0800

    Added bt scripts to package.

--- makeoobe.sh
+++ makeoobe.sh
@@ -1,4 +1,4 @@
-VER_NUM="0.1"
+VER_NUM="0.2"
 OOBE_DIR="tibbo-oobe-${VER_NUM}"
 DEBEMAIL="support@tibbo.com"
 DEBFULLNAME="Tibbo Technology Inc."
@@ -15,6 +15,7 @@ dh_make --indep --createorig
 
 touch debian/install
 cat ../install_wifi >> debian/install 
+cat ../install_bt >> debian/install 
 
 dpkg-source --commit
 
