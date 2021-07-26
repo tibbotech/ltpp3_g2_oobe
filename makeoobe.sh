47adcbda4cfae82a97cd8b9ce735a19ba9ac8c56
Luis Ramirez <luis@tibbo.com>
2021-07-26 09:44:11 +0000

    OOBE added I2C, SPI, TPD with user permissions.

--- makeoobe.sh
+++ makeoobe.sh
@@ -1,4 +1,4 @@
-VER_NUM="0.3.4"
+VER_NUM="0.3.5"
 OOBE_DIR="tibbo-oobe-${VER_NUM}"
 DEBEMAIL="support@tibbo.com"
 DEBFULLNAME="Tibbo Technology Inc."
@@ -19,21 +19,19 @@ cp -R ../wifi ./wifi
 cp -R ../bt ./bt
 cp -R ../tpd ./tpd
 cp -R ../ntios ./ntios
+cp -R ../modules ./modules
 dh_make --indep --createorig
 
 touch debian/install
 cat ../install_wifi >> debian/install 
 cat ../install_bt >> debian/install 
-cat ../install_tpd >> debian/install 
+cat ../install_modules >> debian/install 
 cat ../install_ntios >> debian/install 
 yes | cp ../rules debian/rules 
 yes | cp ../control debian/control 
 yes | cp ../copyright debian/copyright 
 yes | cp ../tibbo-oobe.postinst debian/tibbo-oobe.postinst 
 yes | cp ../tibbo-oobe.postrm debian/tibbo-oobe.postrm 
-
-
-cp ../tpd/tibbo-oobe.tpd.service debian/
 
 dpkg-source --commit
 
